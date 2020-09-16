fa2a22e130e1a20b552362e2e74625630df2e333
username <54479676+CormacKrum@users.noreply.github.com>
2020-09-16 13:31:15 +0530

    Initial Commit

--- game.js
+++ game.js
@@ -11,39 +11,66 @@ const config = {
         update : update
     }
 };
-
+let sky, jet, cursors,ammo,bomb;
 const game = new Phaser.Game(config);
 
 function preload ()
 {
-    this.load.setBaseURL('http://labs.phaser.io');
-
-    this.load.image('sky', 'assets/skies/space3.png');
-    this.load.image('logo', 'assets/sprites/phaser3-logo.png');
-    this.load.image('red', 'assets/particles/red.png');
+    this.load.image('sky', 'http://labs.phaser.io/assets/skies/space3.png');
+    this.load.image('jet','assets/jet.png');
+    this.load.image('bomb','assets/bomb.png');
+    this.load.image('ammo','assets/ammo.png');
+    this.load.image('coin','assets/coin.png');
 }
-
 function create ()
 {
-    this.add.image(400, 300, 'sky');
+    sky = this.add.image(400, 300, 'sky');
+    jet = this.physics.add.image(400,500,'jet').setScale(0.15);
+    jet.setCollideWorldBounds(true);
+    cursors = this.input.keyboard.createCursorKeys();
+    this.input.on('pointerdown',shoot,this);
 
-    const particles = this.add.particles('red');
-
-    const emitter = particles.createEmitter({
-        speed: 100,
-        scale: {start: 1, end: 0},
-        blendMode: 'ADD'
+    bomb = this.physics.add.group({
+        key:'bomb',
+        repeat:3,
+        setXY:{
+            X:20,
+            Y:50,
+            stepX: Phaser.Math.Between(10, config.width - 15),
+            stepY: Phaser.Math.Between(15, 300)
+        }
     });
-
-    const logo = this.physics.add.image(400, 100, 'logo');
-
-    logo.setVelocity(100, 200);
-    logo.setBounce(1, 1);
-    logo.setCollideWorldBounds(true);
-
-    emitter.startFollow(logo);
 }
 
+function shoot(){
+    ammo = this.physics.add.image(jet.x,jet.y,'ammo').setScale(0.1);
+    ammo.setRotation(-Phaser.Math.PI2/4);
+    ammo.setVelocityY(-600);
+    this.physics.add.collider(ammo,bomb,destroyBomb,null,this);
+}
+
+function destroyBomb(bomb,ammo) {
+    bomb.disableBody(true,true);
+    ammo.disableBody(true,true);
+}
 function update() {
-    
+    if (cursors.left.isDown){
+        jet.setVelocityX(-150);
+    }
+    else if (cursors.right.isDown){
+        jet.setVelocityX(150);
+    }
+    else{
+        jet.setVelocityX(0);
+    }
+
+    if (cursors.up.isDown){
+        jet.setVelocityY(-150);
+    }
+    else if (cursors.down.isDown){
+        jet.setVelocityY(150);
+    }
+    else{
+        jet.setVelocityY(0);
+    }
 }
